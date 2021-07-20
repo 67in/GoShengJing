1e04a363f14bb36f484597b393c9559ffaf621c6
liuqin <liuqin3@360.com>
2021-07-20 14:07:52 +0800

    update .gitignore

--- 1/1.2.go
+++ 1/1.2.go
@@ -3,28 +3,27 @@ package main
 import (
 	"fmt"
 	"os"
-	"strconv"
 )
 //
-//func main() {
-//	var s, sep string
-//	for i := 1; i < len(os.Args); i++ {
-//		s += sep + os.Args[i]
-//		sep = " "
-//	}
-//	fmt.Println(s)
-//}
-
-func main(){
-	//var s string
-	//for _, arg := range os.Args[1:]{
-	//	s += sep + arg
-	//	sep = " "
-	//}
-	//s = os.Args[0]
-
-	for i, arg := range os.Args[1:]{
-		fmt.Println(strconv.Itoa(i) +" "+ arg)
+func main() {
+	var s, sep string
+	for i := 1; i < len(os.Args); i++ {
+		s += sep + os.Args[i]
+		sep = " "
 	}
-	//fmt.Println(s)
+	fmt.Println(s)
 }
+
+//func main(){
+//	//var s string
+//	//for _, arg := range os.Args[1:]{
+//	//	s += sep + arg
+//	//	sep = " "
+//	//}
+//	//s = os.Args[0]
+//
+//	for i, arg := range os.Args[1:]{
+//		fmt.Println(strconv.Itoa(i) +" "+ arg)
+//	}
+//	//fmt.Println(s)
+//}
